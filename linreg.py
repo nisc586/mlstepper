81f1ade62e6d885254b14cb4d0956b96df7acfcb
nisc586 <49026680+nisc586@users.noreply.github.com>
2022-07-09 20:46:34 +0200

    Extract a class for linear regression

--- linreg.py
+++ linreg.py
@@ -2,27 +2,52 @@ import numpy as np
 from numpy import linalg
 import matplotlib.pyplot as plt
 
-x_old = np.array([10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5]).reshape(-1, 1)
-x = np.hstack([np.ones((11, 1)), x_old])
-y = np.array([8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68]).reshape(-1, 1)
+# x = [10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5]
+# y = [8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68]
 
-# linear model:
-# y_i = xT_i * beta + eps_i
+class LinearRegression:
+    def __init__(self, x, y, regressors=None):
+        # linear model:
+        # y_i = xT_i * beta + eps_i
+        # Create two column vectors with the observed data
+        self.independent_var = np.array(x).reshape(-1, 1)
+        self.dependent_var = np.array(y).reshape(-1, 1)
 
-# solve ordinary least squares
-coeffs = linalg.inv(x.T@x) @ x.T @ y
-estimates = x @ coeffs
+        if regressors is None:
+            self.regressors = [lambda x: 1, lambda x: x]
+        
+        self.x = np.hstack([
+            np.vectorize(f)(self.independent_var)
+            for f in self.regressors
+        ])
 
-# 
-rss = np.sum((estimates - y.mean())**2)  # "explained" variance
-tss = np.sum((y - y.mean())**2)  # "total" variance
-r_square = 1 - rss/tss  # coefficient of determination
 
-print(r_square)
-fig, ax = plt.subplots()
-ax.set_xlim(0, 15)
-ax.set_ylim(0, 15)
-plt.scatter(x_old, y)
-plt.plot(x_old, estimates, color="red")
+    def solve(self):
+        # solve ordinary least squares
+        y = self.dependent_var
 
-plt.show()
+        self.coeffs = linalg.inv(self.x.T @ self.x) @ self.x.T @ y
+        self.estimates = self.x @ self.coeffs
+
+        self.rss = np.sum((self.estimates - y.mean())**2)  # "explained" variance
+        self.tss = np.sum((y - y.mean())**2)  # "total" variance
+        self.r_square = 1 - self.rss/self.tss  # coefficient of determination
+    
+
+    def show(self, ax=None):
+        print("Coefficients:", self.coeffs)
+        print("R^2:", self.r_square)
+
+        if ax is None:
+            fig, ax = plt.subplots()
+        plt.scatter(self.independent_var, self.dependent_var)
+        plt.scatter(self.independent_var, self.estimates, color="red")
+
+        plt.show()
+    
+
+    def predict(self, x):
+        return sum(
+            beta * f(x)
+            for beta, f in zip(self.coeffs, self.regressors)
+        )
